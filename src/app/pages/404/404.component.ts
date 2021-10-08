739f59e7e483312f641d359eb93786a9302113a3
julyanpatricio <julyan.patricio@gmail.com>
2021-10-08 19:19:45 -0300

    add 404 page

---
+++ src/app/pages/404/404.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core"
+
+@Component({
+  selector: 'app-404',
+  templateUrl: './404.component.html',
+  styleUrls: ['./404.component.scss']
+})
+
+export class Component404 implements OnInit {
+  constructor() {}
+
+  ngOnInit():void {}
+}
