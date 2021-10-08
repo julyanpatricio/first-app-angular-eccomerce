ce6ae415c325a282eb095639078c5a6b322b6875
julyanpatricio <julyan.patricio@gmail.com>
2021-10-08 19:18:16 -0300

    change app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component404 } from './pages/404/404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'404', component: Component404},
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
+  {path:'**', redirectTo:'404',pathMatch:'full'} //cuando no encuentra la ruta, redirige a demo
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { Component404 } from './pages/404/404.component';
+import {HttpClientModule} from '@angular/common/http'
+import { CartComponent } from './shared/components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Component404,
+    CartComponent
   ],
-  imports: [
+  imports: [ //aca van todos los modulos que voy a injectar
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // exports:[] //componentes que vamos a exponer a otros modulos
 })
 export class AppModule { }
